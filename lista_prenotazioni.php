523c7e1112182cdfb0d4b143da1a15060628cddd
AlessandroMaiello <alessandro.maiell0@libero.it>
2021-03-16 12:58:05 +0100

    add table on listaprenotazioni.php

--- lista_prenotazioni.php
+++ lista_prenotazioni.php
@@ -2,6 +2,14 @@
 
 //include il file config.php, che ha al suo interno il collegamento al db
 include_once "config.php";
+require 'vendor/autoload.php';
+
+use League\Plates\Engine;
+
+//creami il motore di template cosi quando chiamo la funzione render andrà a cercare i file .tpl
+//viene creato l'oggetto per la gestione dei template.
+
+$templates = new Engine('./view','tpl');
 
 //Query di inserimento preparata
 $sql = "SELECT * FROM prenotazioni";
@@ -9,6 +17,7 @@ $sql = "SELECT * FROM prenotazioni";
 $stmt = $pdo->query($sql);
 
 $result = $stmt->fetchAll();
-echo "<pre>";
-var_dump($result);
-echo "</pre>";
+
+
+//Rendo un template che mi visualizza la tabella
+echo $templates->render('lista_prenotazioni', ['result' => $result]);
